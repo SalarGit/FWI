8a2c353d2d63d13f731f934122f2d164420c58de
mrcocchieman <salar.ali@Live.nl>
2024-06-07 17:13:57 +0200

    start header

--- full-waveform-inversion/src/App.jsx
+++ full-waveform-inversion/src/App.jsx
@@ -2,20 +2,22 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
-import './Index.css'
+import './index.css'
 
 import Header from './components/Header.jsx';
 
 function App() {
         // const [count, setCount] = useState(0)
-
+        // bg-[#F4F6FB]
   return (
-        <div className='flex flex-col space-y-6 min-h-screen bg-[#F4F6FB]'>
+        <div className='flex flex-col min-h-screen bg-[#F4F6FB]
+            px-14'
+        >
             {/* Header */}
-            {/* <Header /> */}
-            {/* <div className='bg-black min-w-screen'> */}
+            <Header />
+            {/* <div className='bg-black min-w-screen min-h-[120px]'> */}
 
-            </div>
+            {/* </div> */}
             {/* Settings */}
 
             {/* I/O */}
--- full-waveform-inversion/src/components/Header.jsx
+++ full-waveform-inversion/src/components/Header.jsx
@@ -1,11 +1,30 @@
 // import '../App.css'
-import '../Index.css'
+import '../index.css'
+import logo from '../assets/fwi-logo.png'
 
 export default function Header() {
     return (
+        // Header Container
         // fix 120px
-        <div className="min-w-screen h-[120px] bg-black">
+        <div className='flex items-center justify-between min-w-screen min-h-[120px]'
+        >
+            {/* Logo & Title Container */}
+            <div className='flex items-center'>
+            {/* Logo */}
+            <img src={logo} alt="fwi_logo.png"
+                className="w-11 h-11" 
+            />
 
+            {/* Title */}
+            {/* Fix text-5xl to 48px->44px. Fix 'general sans variable' font. */}
+            <p className='ml-6 text-[44px] font-semibold'>Full Waveform Inversion</p>
+            </div>
+
+            {/* Buttons Container*/}
+            <div className='flex space-x-4'>
+                <button>Compare runs</button>
+                <button>Add new run</button>
+            </div>
         </div>
     )
 }
