55bc382911fd28ef94288b53c9eef281ad7cf196
mrcocchieman <salar.ali@Live.nl>
2024-06-07 12:33:53 +0200

    some stuff

---
+++ full-waveform-inversion/src/components/Header.jsx
@@ -0,0 +1,11 @@
+// import '../App.css'
+import '../Index.css'
+
+export default function Header() {
+    return (
+        // fix 120px
+        <div className="min-w-screen h-[120px] bg-black">
+
+        </div>
+    )
+}
