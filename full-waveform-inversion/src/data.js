67263bc0cd2dca5517f630e0b8106e6a01238fe1
mrcocchieman <salar.ali@Live.nl>
2024-07-11 16:07:30 +0200

    Create data.js

---
+++ full-waveform-inversion/src/data.js
@@ -0,0 +1,32 @@
+// Forward Models
+export const forwardModels = ['FiniteDifference', 'Integral']
+
+export const finiteDifference = {
+    
+}
+export const integral = {
+    
+}
+
+// Minimisation Models
+export const minimisationModels = ['gradientDescent', 'conjugateGradient', 'evolution', 'Integral', 'random', 'particleSwarm', 'proportional']
+
+export const gradientDescent = {
+    
+}
+export const conjugateGradient = {
+    n,
+    tolerance,
+}
+export const evolution = {
+    
+}
+export const random = {
+    
+}
+export const particleSwarm = {
+    
+}
+export const proportional = {
+    
+}
