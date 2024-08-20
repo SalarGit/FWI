5d61e49dcfdd00f13bd9e371c156b1ad93d1252b
mrcocchieman <salar.ali@Live.nl>
2024-08-20 15:50:17 +0200

    Create Runs Table

--- full-waveform-inversion/src/components/header/sections/HistoryRuns.jsx
+++ full-waveform-inversion/src/components/header/sections/HistoryRuns.jsx
@@ -9,6 +9,9 @@ import H1 from '../../custom/headings/H1';
 
 export default function HistoryRuns({ onClose }) {
 
+    const th = 'text-start text-sm font-medium';
+    const td = '';
+
     return (
         <>
             {/* Blur background */}
@@ -18,10 +21,12 @@ export default function HistoryRuns({ onClose }) {
             {/* 1746px width because border is outside */}
             {/* flex items-center justify-center */}
             <div className='absolute top-0 left-0 z-30 size-full flex justify-center items-center'> {/* Disabled: #B6B7BE*/}
-                <div className='relative w-[1746px] h-[952px]
-                    bg-white border border-[#D7DFFF] rounded-2xl'
-                >
-                    <div className='flex items-center justify-between px-6 py-[26px]'>
+                {/* Container to put upper & lower beneath each other */}
+                <div className='flex flex-col'>
+                    {/* Upper part */}
+                    <div className='flex items-center justify-between px-6 py-[26px]
+                        w-[1746px] h-[81px] bg-white border border-[#D7DFFF] rounded-t-3xl'
+                        >
                         <H1 heading='Mode:'/>
                         <button className='hover:bg-[#F1F4FF] duration-200 rounded-md'
                             onClick={onClose}
@@ -29,9 +34,158 @@ export default function HistoryRuns({ onClose }) {
                             <img src={closeBig} alt="close-big.png" />
                         </button>
                     </div>
-                    <BorderTop />
 
-                    {/* <UpperPart heading='Mode:' styling='px-6 py-4'>
+                    {/* Lower part */}
+                    <div className='w-[1746px] h-[873px] bg-[#F4F6FB] border border-t-0 border-[#D7DFFF] rounded-b-3xl p-6'>
+                        {/* <div className='flex flex-col justify-between'>
+                            <div className='flex space-x-4 justify-between'>
+                                <p>Run name</p>
+                                <p>Grid size</p>
+                                <p>Forward model</p>
+                            </div>
+                            <div className='flex space-x-4 justify-between'>
+                                <p>Data Analysis Run - 01-03-2023</p>
+                                <p>62 x 32</p>
+                                <p>Integral</p>
+                            </div>
+                            <div className='flex space-x-4'>
+                                <p>Data Analysis Run - 01-03-2023</p>
+                                <p>62 x 32</p>
+                                <p>FiniteDifferenceMPI</p>
+                            </div>
+                        </div> */}
+                        <table class="table-auto w-full py-[18px] px-6
+                            bg-white border border-[#D7DFFF] border-separate rounded-2xl"
+                        >
+                            <thead className='border border-black'>
+                                <tr className=''>
+                                    <th className={`${th}`}>Run name</th>
+                                    <th className={th}>Forward model</th>
+                                    <th className={th}>Grid size</th>
+                                    <th className={th}>Minimisation model</th>
+                                    <th className={th}>Threads</th>
+                                    <th className={th}>Location on disk</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Data Analysis Run - 01-03-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>Integral</td>
+                                    <td>Evolution</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Data Quality Assessment - 05-03-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>FiniteDifferenceMPI</td>
+                                    <td>Proportional</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Velocity Model Assessment - 10-04-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>FiniteDifferenceMPI</td>
+                                    <td>Random</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Seismic Metrics Calculation - 15-04-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>FiniteDifference</td>
+                                    <td>ConjugateGradient</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Quality Assessment Analysis - 17-04-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>Integral</td>
+                                    <td>Evolution</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Parameter Optimization - 25-07-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>FiniteDifferenceOpenMP</td>
+                                    <td>GradientDescent</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Tomographic Model Generation - 18-08-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>Integral</td>
+                                    <td>Evolution</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                                <tr>
+                                    <td>Seismic Data Analysis - 19-09-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>Integral</td>
+                                    <td>ParticleSwarm</td>
+                                    <td>1</td>
+                                    <td>/home/tony_stark/default/fwi</td>
+                                </tr>
+                            </tbody>
+                            </table>
+                        {/* <table className='table-auto'>
+                            <thead>
+                                <tr>
+                                    <th>Run name</th>
+                                    <th>Grid size</th>
+                                    <th>Forward model</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Data Analysis Run - 01-03-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>Integral</td>
+                                </tr>
+                                    <td>Data Analysis Run - 05-07-2023</td>
+                                    <td>62 x 32</td>
+                                    <td>FiniteDifferenceMPI</td>
+                            </tbody>
+                        </table> */}
+                    </div>
+                    {/* 873px */}
+
+                    {/* <div className='relative w-[1746px] h-[952px]
+                        bg-white border border-[#D7DFFF] rounded-2xl'
+                    >
+                        <div className='flex items-center justify-between px-6 py-[26px]'>
+                            <H1 heading='Mode:'/>
+                            <button className='hover:bg-[#F1F4FF] duration-200 rounded-md'
+                                onClick={onClose}
+                            >
+                                <img src={closeBig} alt="close-big.png" />
+                            </button>
+                        </div>
+                        <BorderTop />
+
+                        
+                    </div> */}
+                </div>
+            </div>
+        </>
+    )
+}
+
+// Class
+// Properties
+// static	position: static;
+// fixed	position: fixed;
+// absolute	position: absolute;
+// relative	position: relative;
+// sticky
+
+{/* <UpperPart heading='Mode:' styling='px-6 py-4'>
                         <div className='flex'>
                             <div>bruh</div>
                             <div className='flex space-x-6'>
@@ -51,17 +205,3 @@ export default function HistoryRuns({ onClose }) {
                             </div>
                         </div>
                     </UpperPart> */}
-                </div>
-                
-            </div>
-        </>
-    )
-}
-
-// Class
-// Properties
-// static	position: static;
-// fixed	position: fixed;
-// absolute	position: absolute;
-// relative	position: relative;
-// sticky
